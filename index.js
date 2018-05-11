2c7b30fd49973d4e5ec524cec0df7ea67aa67bb7
Austin Cheng <austinbicheng@gmail.com>
2018-05-11 13:59:05 -0700

    gonna crash

--- index.js
+++ index.js
@@ -11,36 +11,41 @@ const multipartyOptions = {
 let nocache = require('nocache')
 app.use(nocache())
 
-let mimeSafeSend = (filename, res) => {
+let mimeSafeSend = (filename, res) =>
+{
   // mime type
   let type = mime.getType(filename);
-
   // header fields
-  if (!res.getHeader('content-type')) {
+  if (!res.getHeader('content-type'))
+  {
     var charset = null; //mime.getType(type);
     res.setHeader('Content-Type', type + (charset ? '; charset=' + charset : ''));
   }
   res.sendFile(filename);
 }
 
-app.engine('.hbs', exphbs({
+app.engine('.hbs', exphbs(
+{
   extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
 let path = require('path');
 let aVar = 0;
 
-app.get('/webs/game.js', function(req, res) {
+app.get('/webs/game.js', function(req, res)
+{
   mimeSafeSend(path.join(__dirname, '/webs/game.js'), res)
 })
 
-app.get('/webs/controls.js', function(req, res) {
+app.get('/webs/controls.js', function(req, res)
+{
   mimeSafeSend(path.join(__dirname, '/webs/controls.js'), res)
 })
 
 app.set('views', path.join(__dirname, 'webs'));
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) =>
+{
   res.render("website")
 })
 
@@ -49,8 +54,11 @@ let io = SocketIO.listen(server);
 let playerIds = [];
 let players = {};
 let enemys = [];
+let dead = [];
+let img = ""
 
-io.on('connection', function(socket) {
+io.on('connection', function(socket)
+{
   console.log('a user connected');
   players[socket.id] = {
     id: socket.id,
@@ -61,30 +69,36 @@ io.on('connection', function(socket) {
     color: 'rgb(0,0,0)'
   };
   playerIds.push(socket.id)
-  socket.on('movement', function(data) {
-    var player = players[socket.id] || {};
-    if (data.left) {
+  socket.on('movement', function(data)
+  {
+    var player = players[socket.id] ||
+    {};
+    if (data.left)
+    {
       player.x -= 5;
       if (player.x < 0)
       {
         player.x = 0;
       }
     }
-    if (data.up) {
+    if (data.up)
+    {
       player.y -= 5;
       if (player.y < 0)
       {
         player.y = 0;
       }
     }
-    if (data.right) {
+    if (data.right)
+    {
       player.x += 5;
       if (player.x > 800)
       {
         player.x = 800;
       }
     }
-    if (data.down) {
+    if (data.down)
+    {
       player.y += 5;
       if (player.y > 600)
       {
@@ -92,22 +106,29 @@ io.on('connection', function(socket) {
       }
     }
   })
-  socket.on('setName', function(data) {
+  socket.on('setName', function(data)
+  {
     players[socket.id].name = data;
   })
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function()
+  {
     delete players[socket.id];
     playerIds.splice(playerIds.indexOf(socket.id), 1);
   })
-  setInterval(function() {
+  setInterval(function()
+  {
     io.emit('playerState', players)
     io.emit('enemyState', enemys)
+    io.emit('deadPlayers', dead)
   }, 1000 / 60)
 });
 
-setInterval(function() {
-  if (enemys.length < 20 && playerIds.length > 0) {
-    enemys.push({
+setInterval(function()
+{
+  if (enemys.length < 20 && playerIds.length > 0)
+  {
+    enemys.push(
+    {
       hitDes: true,
       id: enemys.length,
       ai: Math.floor(Math.random() * 3),
@@ -119,29 +140,40 @@ setInterval(function() {
   }
 }, 2000);
 
-function updateEnemies() {
-  for (let i = 0; i < enemys.length; i++) {
+function updateEnemies()
+{
+  for (let i = 0; i < enemys.length; i++)
+  {
 
-    if (enemys[i].ai == 0) {
+    if (enemys[i].ai == 0)
+    {
       enemys[i].x = enemys[i].x + Math.random() * 10 - 5;
       enemys[i].y = enemys[i].y + Math.random() * 10 - 5;
-    } else if (enemys[i].ai == 1) {
-      if(playerIds.length > 0)
+    }
+    else if (enemys[i].ai == 1)
+    {
+      if (playerIds.length > 0)
       {
-      if (!('stalk' in enemys[i]) || playerIds.indexOf(enemys[i].stalk) == -1) {
-        enemys[i].stalk = playerIds[Math.floor(Math.random() * playerIds.length)]
+        if (!('stalk' in enemys[i]) || playerIds.indexOf(enemys[i].stalk) == -1)
+        {
+          enemys[i].stalk = playerIds[Math.floor(Math.random() * playerIds.length)]
+        }
+        let tx = players[enemys[i].stalk].x - enemys[i].x,
+          ty = players[enemys[i].stalk].y - enemys[i].y
+        dist = Math.sqrt(tx * tx + ty * ty);
+
+        if (dist > 10)
+        {
+          enemys[i].y = enemys[i].y + enemys[i].spd * ty / dist;
+          enemys[i].x = enemys[i].x + enemys[i].spd * tx / dist;
+        }
       }
-      let tx = players[enemys[i].stalk].x - enemys[i].x,
-        ty = players[enemys[i].stalk].y - enemys[i].y
-      dist = Math.sqrt(tx * tx + ty * ty);
 
-      if (dist > 10) {
-        enemys[i].y = enemys[i].y + enemys[i].spd * ty / dist;
-        enemys[i].x = enemys[i].x + enemys[i].spd * tx / dist;
-      }}
-
-    } else if (enemys[i].ai == 2) {
-      if (enemys[i].hitDes) {
+    }
+    else if (enemys[i].ai == 2)
+    {
+      if (enemys[i].hitDes)
+      {
         enemys[i].tx = Math.floor(Math.random() * 600)
         enemys[i].ty = Math.floor(Math.random() * 600)
         enemys[i].hitDes = false;
@@ -150,21 +182,31 @@ function updateEnemies() {
         ty = enemys[i].ty - enemys[i].y
       dist = Math.sqrt(tx * tx + ty * ty);
 
-      if (dist > 10) {
+      if (dist > 10)
+      {
         enemys[i].y = enemys[i].y + enemys[i].spd * ty / dist;
         enemys[i].x = enemys[i].x + enemys[i].spd * tx / dist;
-      } else {
+      }
+      else
+      {
         enemys[i].hitDes = true;
       }
     }
-    for (let j = 0; j < playerIds.length; j++) {
-      if (10 > Math.sqrt((players[playerIds[j]].x - enemys[i].x) * (players[playerIds[j]].x - enemys[i].x) + (players[playerIds[j]].y - enemys[i].y) * (players[playerIds[j]].y - enemys[i].y))) {
+    for (let j = 0; j < playerIds.length; j++)
+    {
+      if (10 > Math.sqrt((players[playerIds[j]].x - enemys[i].x) * (players[playerIds[j]].x - enemys[i].x) + (players[playerIds[j]].y - enemys[i].y) * (players[playerIds[j]].y - enemys[i].y)))
+      {
         players[playerIds[j]].color = "rgb(200,25,25)";
-        }
+        dead.push({x: players[playerIds[j]].x,
+                  y: players[playerIds[j]].y,
+                }
+                )
       }
     }
   }
+}
 
-  setInterval(function() {
-    updateEnemies();
-  }, 1000 / 30);
+setInterval(function()
+{
+  updateEnemies();
+}, 1000 / 30);
--- webs/game.js
+++ webs/game.js
@@ -5,39 +5,53 @@ let context = canvas.getContext('2d');
 
 let players = {};
 let enemys = [];
+let dead = [];
 
 
-socket.on('playerState', function(playersrec) {
-  for (let id in playersrec) {
+socket.on('playerState', function(playersrec)
+{
+  for (let id in playersrec)
+  {
     let player = playersrec[id];
     players[id] = player;
   }
 });
 
-socket.on('enemyState', function(enemysrec) {
+socket.on('enemyState', function(enemysrec)
+{
   enemys = enemysrec;
 });
+
+socket.on('deadPlayers', function(deaded)
+{
+  dead = deaded;
+})
 
 let submitButton = document.getElementById('submit')
 let inputBox = document.getElementById('inputBox')
 
-submitButton.addEventListener('click', function(event) {
+submitButton.addEventListener('click', function(event)
+{
   event.preventDefault();
   socket.emit('setName', inputBox.value)
 })
 
-inputBox.addEventListener('keyup', function(event) {
-  if (event.keyCode == 13) {
+inputBox.addEventListener('keyup', function(event)
+{
+  if (event.keyCode == 13)
+  {
     event.preventDefault();
     socket.emit('setName', inputBox.value);
   }
 })
 
-setInterval(function() {
+setInterval(function()
+{
   context.clearRect(0, 0, 800, 600);
   context.fillStyle = 'black';
-  context.strokeRect(0,0,800,600);
-  for (let id in players) {
+  context.strokeRect(0, 0, 800, 600);
+  for (let id in players)
+  {
     let player = players[id];
     context.fillStyle = player.color;
     context.beginPath();
@@ -46,12 +60,26 @@ setInterval(function() {
     context.fillText(player.name, player.x, player.y + 15)
   }
   let bleh = 0;
-  while (bleh < enemys.length) {
+  while (bleh < enemys.length)
+  {
     let enemy = enemys[bleh];
     bleh++;
     context.fillStyle = enemy.color;
     context.beginPath();
     context.arc(enemy.x, enemy.y, 10, 0, 2 * Math.PI);
     context.fill();
+  }
+  bleh = 0;
+  while (bleh < dead.length)
+  {
+    context.fillStyle = "red"
+    let enemy = dead[bleh];
+    bleh++;
+    context.beginPath();
+    context.moveTo(enemy.x - 20, enemy.y - 20);
+    context.lineTo(enemy.x + 20, enemy.y + 20);
+    context.moveTo(enemy.x + 20, enemy.y - 20);
+    context.lineTo(enemy.x - 20, enemy.y + 20);
+    context.stroke();
   }
 }, 1000 / 60)
